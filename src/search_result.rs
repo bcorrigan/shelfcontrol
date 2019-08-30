daf99908e8ae72350b3ef828d40093fd03995c75
Barry Corrigan <b.j.corrigan@gmail.com>
2019-08-30 23:47:22 +0100

    Non-error serialisation will go here

---
+++ src/search_result.rs
@@ -0,0 +1,37 @@
+use BookMetadata;
+//Responsible for representing search results, serialising into variopus formats etc
+
+#[derive(Debug)]
+pub struct SearchResult {
+    pub count: usize,
+    pub start: usize,
+    pub query: String,
+    pub books: Vec<BookMetadata>,
+}
+
+impl SearchResult {
+    pub fn to_json(&self) -> String {
+        let mut json_str: String = format!(
+			"{{\"count\":{}, \"position\":{}, \"query\":\"{}\", \"books\":[",
+			self.count,
+			self.start,
+			self.query.replace("\"", "\\\"")
+		).to_string();
+
+        let num_books = self.books.len();
+
+        for (i,bm) in self.books.iter().enumerate() {
+            json_str.push_str(match &serde_json::to_string(bm) {
+		        Ok(str) => str,
+			    Err(_) => continue,
+		    });
+
+            if (i + 1) != num_books {
+                json_str.push_str(",");
+            }
+        }
+
+        json_str.push_str("]}");
+        json_str
+    }
+}
