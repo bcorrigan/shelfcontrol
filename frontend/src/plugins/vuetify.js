97a3a1652688e5d768f1db34107cd6d34e38af8f
Barry Corrigan <b.j.corrigan@gmail.com>
2019-12-07 21:46:18 +0000

    Update dependencies, have it work with vuetify 2.0

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
+//import 'vuetify/src/stylus/app.styl'
+import 'vuetify/src/styles/main.sass'
 
 Vue.use(Vuetify, {
   iconfont: 'md',
