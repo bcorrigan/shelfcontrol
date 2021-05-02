62bdc395a222c357a7b4facecf4c278f09d190d0
bcorrigan <b.j.corrigan@gmail.com>
2021-05-02 21:07:58 +0100

    frontend/vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,20 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
+      devServer: {
+        disableHostCheck: true,
+	proxy: {
+		"/api/*": {
+			target: "http://[::1]:8000",
+			secure: false,
+			changeOrigin: true
+	        },
+		"/img/*": {
+			target: "http://[::1]:8000",
+			secure: false,
+			changeOrigin: true
+		}
+        }
+    },
+    transpileDependencies: [
+        'vuetify'
+    ]
 }
