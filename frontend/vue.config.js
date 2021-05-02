963148e4893e6b257a73bd0cd2284281ce716be4
bcorrigan <b.j.corrigan@gmail.com>
2021-05-02 20:55:30 +0100

    Upgrade frontend stuff

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,19 +1,5 @@
-// vue.config.js
 module.exports = {
-    // options...
-    devServer: {
-        disableHostCheck: true,
-	proxy: {
-		"/api/*": {
-			target: "http://[::1]:8000",
-			secure: false,
-			changeOrigin: true
-	        },
-		"/img/*": {
-			target: "http://[::1]:8000",
-			secure: false,
-			changeOrigin: true
-		}
-        }
-    }
+  transpileDependencies: [
+    'vuetify'
+  ]
 }
